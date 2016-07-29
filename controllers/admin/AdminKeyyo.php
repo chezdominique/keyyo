6af3f95f514fdb6f6f867a1b3b3e37d6407f57e5
Devsprite <dominique@chez-dominique.fr>
2016-07-29 08:00:11 +0200

    Keyyo pour Prestashop
    Module non officiel permettant les appels KEYYO depuis le BO de
    Prestashop

---
+++ controllers/admin/AdminKeyyo.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * AdminKeyyo File Doc Comment
+ * AdminKeyyo Class Doc Comment
+ *
+ * 2007-2015 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Dominique <dominique@chez-dominique.fr
+ * @copyright 2007-2016 PrestaShop SA / 2011-2016 Dominique
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * International Registred Trademark & Property of PrestaShop SA
+ */
+class AdminKeyyoController extends ModuleAdminController
+{
+    public function __construct()
+    {
+        if (!defined('_PS_VERSION_')) {
+            exit;
+        }
+        $this->module = 'keyyo';
+        $this->className = 'AdminKeyyoController';
+        $this->lang = false;
+        $this->bootstrap = true;
+        $this->context = Context::getContext();
+        $this->required_fields = array('id_customer', 'phone');
+        $this->table = 'customer';
+        $this->explicitSelect = false;
+        $this->addRowAction('afficher');
+        $this->addCSS(_PS_MODULE_DIR_ . 'keyyo/views/css/adminkeyyo.css');
+        $this->addJquery();
+        $this->addJS(_PS_MODULE_DIR_ . 'keyyo/views/js/adminkeyyo.js');
+        $this->list_no_link = true;
+
+        $this->fields_list = array(
+            'id_customer' => array(
+                'title' => $this->l('ID'),
+                'align' => 'center',
+                'class' => 'fixed-width-xs'),
+            'firstname' => array(
+                'title' => $this->l('Prénom'),
+                'filter_key' => 'a!firstname'),
+            'lastname' => array(
+                'title' => $this->l('Nom'),
+                'filter_key' => 'a!lastname'),
+            'address1' => array(
+                'title' => $this->l('Adresse')),
+            'postcode' => array(
+                'title' => $this->l('Code postal'),
+                'align' => 'right'),
+            'city' => array(
+                'title' => $this->l('Ville')),
+            'phone' => array(
+                'title' => $this->l('Téléphone'),
+                'align' => 'center',
+                'callback' => 'makePhoneCall'
+            ),
+            'phone_mobile' => array(
+                'title' => $this->l('Mobile'),
+                'align' => 'center',
+                'callback' => 'makePhoneCall'
+            )
+        );
+
+        parent::__construct();
+
+        $this->_select =
+            'a.id_customer, a.firstname, a.lastname, ad.address1, ad.postcode, ad.city, ad.phone, ad.phone_mobile';
+        $this->_join = '
+            LEFT JOIN `' . _DB_PREFIX_ . 'address` ad ON a.id_customer = ad.id_customer
+        ';
+    }
+
+    public function makePhoneCall($number, $params)
+    {
+        $phoneNumber = $this->sanityzePhoneNumber($number);
+
+        $keyyo_link = '<a href="' . Context::getContext()->link->getAdminLink('AdminKeyyo') . '&ajax=1&action=KeyyoCall';
+        $keyyo_link .= '&CALLE=' . $phoneNumber;
+        $keyyo_link .= '&CALLE_NAME=' . $params['lastname'] . '_' . $params['firstname'];
+        $keyyo_link .= '" class="keyyo_link">' . $phoneNumber . '</a>';
+
+        return $keyyo_link;
+    }
+
+    public function displayAfficherLink($token, $id)
+    {
+        $tokenLite = Tools::getAdminTokenLite('AdminCustomers');
+        $link =
+            self::$currentIndex . '&controller=AdminCustomers&id_customer=' . $id . '&viewcustomer&token=' . $tokenLite;
+        return '<a href="' . $link . '" class="icon-search-plus"> Afficher</a>';
+    }
+
+    protected function getKeyyoAccount()
+    {
+        $sql = 'SELECT keyyo_account FROM `' . _DB_PREFIX_ . 'employee` 
+        WHERE id_employee = ' . $this->context->employee->id;
+        $keyyo_account = Db::getInstance()->getRow($sql);
+
+        if (!empty($keyyo_account['keyyo_account'])) {
+            $keyyo_number = $keyyo_account['keyyo_account'];
+        } else {
+            $keyyo_number = Configuration::get('KEYYO_ACCOUNT');
+        }
+        return $keyyo_number;
+    }
+
+    public function ajaxProcessKeyyoCall()
+    {
+        $account = $this->getKeyyoAccount();
+        $calle = Tools::getValue('CALLE');
+        $calle_name = Tools::getValue('CALLE_NAME');
+
+        if (!$account) {
+            $return = Tools::jsonEncode(array('msg' => 'Veuillez configurer votre numéro de compte KEYYO.'));
+            die($return);
+        }
+
+        if (!$calle || !$calle_name) {
+            $return = Tools::jsonEncode(array('msg' => 'Il manque une information pour composer le numéro.'));
+            die($return);
+        } else {
+            $keyyo_link = 'https://ssl.keyyo.com/makecall.html?ACCOUNT=' . $account;
+            $keyyo_link .= '&CALLE=' . $calle . '&CALLE_NAME=' . $calle_name;
+
+            $fp = fopen($keyyo_link, 'r');
+
+            if ($fp) {
+                $return = Tools::jsonEncode(array('msg' => 'Appel du ' . $calle . ' en cours.'));
+                die($return);
+            } else {
+                $return = Tools::jsonEncode(array('msg' => 'Problème lors de l\'appel au ' . $calle));
+                die($return);
+            }
+        }
+    }
+
+    private function sanityzePhoneNumber($number)
+    {
+        return $number;
+    }
+}
---
+++ keyyo.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * AdminKeyyo File Doc Comment
+ * AdminKeyyo Class Doc Comment
+ *
+ * 2007-2015 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Dominique <dominique@chez-dominique.fr
+ * @copyright 2007-2016 PrestaShop SA / 2011-2016 Dominique
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * International Registred Trademark & Property of PrestaShop SA
+ */
+class Keyyo extends Module
+{
+    protected $tabName;
+    protected $errors = array();
+    protected $html = '';
+
+    /* Set default configuration values here */
+    protected $config = array(
+        'KEYYO_ACCOUNT' => "",
+    );
+
+    public function __construct()
+    {
+        if (!defined('_PS_VERSION_')) {
+            exit;
+        }
+
+        $this->name = 'keyyo';
+        $this->tab = 'others';
+        $this->version = '1.0.0';
+        $this->author = 'Dominique';
+        $this->need_instance = 0;
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Module non officiel pour la téléphonie KEYYO');
+        $this->description = $this->l('Installe un lien permettant les appels via KEYYO.');
+        $this->confirmUninstall = $this->l('Etes vous sur ?');
+        $this->tabName = 'Keyyo';
+    }
+
+    public function install()
+    {
+        if (!parent::install() or
+            !$this->installConfig() or
+            !$this->alterEmployeeTable() or
+            !$this->createTabs() or
+            !$this->registerHook('displayHeader') or
+            !$this->registerHook('displayBackOfficeHeader')
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    public function uninstall()
+    {
+        if (!parent::uninstall() or
+            !$this->eraseConfig() or
+            !$this->eraseTabs() or
+            !$this->alterEmployeeTable('remove')
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    public function alterEmployeeTable($method = 'add')
+    {
+        if ($method == 'add') {
+            $sql = 'ALTER TABLE ' . _DB_PREFIX_ . 'employee ADD `keyyo_account` BIGINT (64) NULL';
+        } else {
+            $sql = 'ALTER TABLE ' . _DB_PREFIX_ . 'employee DROP COLUMN `keyyo_account`';
+        }
+
+        if (!Db::getInstance()->execute($sql)) {
+            return false;
+        }
+        return true;
+    }
+
+    private function installConfig()
+    {
+        foreach ($this->config as $keyname => $value) {
+            Configuration::updateValue($keyname, $value);
+        }
+        return true;
+    }
+
+
+    private function eraseConfig()
+    {
+        foreach ($this->config as $keyname => $value) {
+            Configuration::deleteByName($keyname);
+        }
+        return true;
+    }
+
+    private function createTabs()
+    {
+        $tab = new Tab();
+        $tab->active = 1;
+        $languages = Language::getLanguages(false);
+        if (is_array($languages)) {
+            foreach ($languages as $language) {
+                $tab->name[$language['id_lang']] = $this->tabName;
+            }
+        }
+        $tab->class_name = 'Admin' . Tools::ucfirst($this->name);
+        $tab->module = $this->name;
+        $tab->id_parent = 0;
+
+        return (bool)$tab->add();
+    }
+
+    private function eraseTabs()
+    {
+        $id_tab = (int)Tab::getIdFromClassName('Admin' . Tools::ucfirst($this->name));
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            $tab->delete();
+        }
+        return true;
+    }
+
+    public function getContent()
+    {
+        $this->postProcess();
+        $this->displayForm();
+
+        return $this->html;
+    }
+
+    private function postProcess()
+    {
+        if (Tools::isSubmit('submitUpdate')) {
+            $keyyo_account = Tools::getValue('keyyo_account');
+
+            $pattern = '/^33[0-9]{9}$/';
+            if (Validate::isString($keyyo_account) &&
+                preg_match($pattern, $keyyo_account)
+            ) {
+                Configuration::updateValue('KEYYO_ACCOUNT', $keyyo_account);
+            } else {
+                $this->errors[] = 'Le numero n\'est pas au bon format.';
+            }
+
+            // Error handling
+            if ($this->errors) {
+                $this->html .= $this->displayError(implode($this->errors, '<br />'));
+            } else {
+                $this->html .= $this->displayConfirmation($this->l('Paramètres mis à jour'));
+            }
+        }
+    }
+
+    private function displayForm()
+    {
+        $this->html .= $this->generateForm();
+    }
+
+    private function generateForm()
+    {
+        $inputs = array();
+        $inputs[] = array(
+            'type' => 'text',
+            'label' => $this->l('Compte par défaut KEYYO.'),
+            'name' => 'keyyo_account',
+            'desc' => 'Veuillez entrer le numéro KEYYO par défaut. (33 suivi des 9 chiffres)',
+            'lang' => false
+        );
+        $fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs'
+                ),
+                'input' => $inputs,
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                    'class' => 'btn btn-default pull-right',
+                    'name' => 'submitUpdate'
+                )
+            )
+        );
+
+        $helper = new HelperForm();
+        $helper->submit_action = 'submitUpdate';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfig()
+        );
+        return $helper->generateForm(array($fields_form));
+    }
+
+    private function getConfig()
+    {
+        return array(
+            'keyyo_account' => Configuration::get('KEYYO_ACCOUNT')
+        );
+    }
+
+    public function hookDisplayBackOfficeHeader()
+    {
+        $this->context->controller->addCSS($this->_path . 'views/css/adminkeyyo.css', 'all');
+    }
+
+    public function hookDisplayHeader()
+    {
+        $this->context->controller->addCSS($this->_path . 'views/css/adminkeyyo.css', 'all');
+        $this->context->controller->addJS($this->_path . 'views/js/adminkeyyo.js', 'all');
+    }
+}
